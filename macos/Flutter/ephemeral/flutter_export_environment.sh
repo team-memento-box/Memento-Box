b6609691c1b714265afbb87e26bb477f06a08feb
Seungyeon Kim <syk.kairos@gmail.com>
2025-06-12 10:54:16 +0900

    Clean up tracked files according to .gitignore

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=E:\flutter"
-export "FLUTTER_APPLICATION_PATH=E:\Memento-Box"
+export "FLUTTER_ROOT=/Users/syk/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/syk/PC/git/Memento-Box"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
